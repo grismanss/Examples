d43c36eb168f22700790eafae8dfdc9693b40975
s.s.grisman@mail.ru <s.s.grisman@mail.ru>
2021-02-27 13:18:49 +0700

    27.02.2021

---
+++ FastReport пример/WindowsFormsApp18/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WindowsFormsApp18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.report1 = new FastReport.Report();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.peopleDataSet = new WindowsFormsApp18.PeopleDataSet();
+            this.nosologyBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nosologyTableAdapter = new WindowsFormsApp18.PeopleDataSetTableAdapters.NosologyTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.naimenovanieDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.report1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peopleDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nosologyBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // report1
+            // 
+            this.report1.NeedRefresh = false;
+            this.report1.ReportResourceString = resources.GetString("report1.ReportResourceString");
+            this.report1.Tag = null;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.naimenovanieDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.nosologyBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(76, 191);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // peopleDataSet
+            // 
+            this.peopleDataSet.DataSetName = "PeopleDataSet";
+            this.peopleDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // nosologyBindingSource
+            // 
+            this.nosologyBindingSource.DataMember = "Nosology";
+            this.nosologyBindingSource.DataSource = this.peopleDataSet;
+            // 
+            // nosologyTableAdapter
+            // 
+            this.nosologyTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // naimenovanieDataGridViewTextBoxColumn
+            // 
+            this.naimenovanieDataGridViewTextBoxColumn.DataPropertyName = "Naimenovanie";
+            this.naimenovanieDataGridViewTextBoxColumn.HeaderText = "Naimenovanie";
+            this.naimenovanieDataGridViewTextBoxColumn.Name = "naimenovanieDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.report1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.peopleDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nosologyBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FastReport.Report report1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PeopleDataSet peopleDataSet;
+        private System.Windows.Forms.BindingSource nosologyBindingSource;
+        private PeopleDataSetTableAdapters.NosologyTableAdapter nosologyTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn naimenovanieDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ FastReport пример/WindowsFormsApp18/Form1.cs
@@ -0,0 +1,128 @@
+﻿using FastReport;
+using FastReport.Data;
+using FastReport.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Report report = new Report();
+
+            // register the "Products" table 
+            report.RegisterData(peopleDataSet.Tables["Nosology"], "Nosology");
+            // enable it to use in a report 
+            report.GetDataSource("Nosology").Enabled = true;
+       
+            // create A4 page with all margins set to 1cm 
+            ReportPage page1 = new ReportPage(); page1.Name = "Page1"; report.Pages.Add(page1);
+            page1.LeftMargin = 0;
+            page1.RightMargin = 0;
+            page1.TopMargin = 0;
+            page1.BottomMargin = 0;
+            // create ReportTitle band 
+            page1.ReportTitle = new ReportTitleBand(); page1.ReportTitle.Name = "ReportTitle1";
+            // set its height to 1.5cm
+            page1.ReportTitle.Height = Units.Centimeters * 1.5f;
+            
+            // create group header
+            GroupHeaderBand group1 = new GroupHeaderBand(); group1.Name = "GroupHeader1";
+            group1.Height = Units.Centimeters * 1;
+            
+            // set group condition
+            group1.Condition = "[Nosology.Naimenovanie].Substring(0, 1)";
+            // add group to the page.Bands collection 
+            page1.Bands.Add(group1);
+
+            // create group footer
+            group1.GroupFooter = new GroupFooterBand(); 
+            group1.GroupFooter.Name = "GroupFooter1"; 
+            group1.GroupFooter.Height = Units.Centimeters * 1;
+
+            // create DataBand
+            DataBand data1 = new DataBand(); 
+            data1.Name = "Data1";
+            data1.Height = Units.Centimeters * 0.5f;
+            // set data source
+            data1.DataSource = report.GetDataSource("Nosology");
+            // connect databand to a group 
+            group1.Data = data1;
+
+
+            PictureObject picture = new PictureObject();
+            //picture.ForceLoadImage("C://Users//Svetlana//Desktop//1.jpg");
+
+
+            picture.Bounds = new RectangleF(0, 0, Units.Centimeters * 21, Units.Centimeters * 20); //Set object bounds
+            picture.Image = new Bitmap("C://Users//Svetlana//Desktop//1.jpg"); //Set picture
+            page1.ReportTitle.Objects.Add(picture);
+            //picture.Parent = data; //Set picture parent object
+            //picture.CreateUniqueName();
+            // create "Text" objects
+
+            // report title
+            TextObject text1 = new TextObject(); text1.Name = "Text1";
+            // set bounds
+            text1.Bounds = new RectangleF(0, 0, Units.Centimeters * 19, Units.Centimeters * 1);
+            // set text
+            text1.Text = "Nosologys";
+            // set appearance
+            text1.HorzAlign = HorzAlign.Center;
+            text1.Font = new Font("Tahoma", 14, FontStyle.Bold);
+            // add it to ReportTitle 
+            page1.ReportTitle.Objects.Add(text1);
+
+            // group
+            TextObject text2 = new TextObject(); 
+            text2.Name = "Text2";
+            text2.Bounds = new RectangleF(0, 0, Units.Centimeters * 2, Units.Centimeters * 1);
+            text2.Text = "[[Nosology.Naimenovanie].Substring(0, 1)]"; text2.Font = new Font("Tahoma", 10, FontStyle.Bold);
+            // add it to GroupHeader 
+            group1.Objects.Add(text2);
+
+            // data band
+            TextObject text3 = new TextObject(); text3.Name = "Text3";
+            text3.Bounds = new RectangleF(0, 0, Units.Centimeters * 10, Units.Centimeters * 0.5f);
+            text3.Text = "[Nosology.Naimenovanie]"; text3.Font = new Font("Tahoma", 8);
+            // add it to DataBand 
+            data1.Objects.Add(text3);
+
+            // group footer
+            TextObject text4 = new TextObject(); text4.Name = "Text4";
+            text4.Bounds = new RectangleF(0, 0, Units.Centimeters * 10, Units.Centimeters * 0.5f);
+            text4.Text = "Count: [CountOfProducts]";
+            text4.Font = new Font("Tahoma", 8, FontStyle.Bold);
+            // add it to GroupFooter 
+            group1.GroupFooter.Objects.Add(text4);
+
+            // add a total
+            Total groupTotal = new Total(); groupTotal.Name = "CountOfProducts"; groupTotal.TotalType = TotalType.Count; groupTotal.Evaluator = data1; groupTotal.PrintOn = group1.Footer;
+            // add it to report totals 
+            report.Dictionary.Totals.Add(groupTotal);
+            report.Show();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "peopleDataSet.Nosology". При необходимости она может быть перемещена или удалена.
+            this.nosologyTableAdapter.Fill(this.peopleDataSet.Nosology);
+
+        }
+    }
+}
